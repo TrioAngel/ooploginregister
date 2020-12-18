3793f1e989c3eeb45599ae6c83a5c9c042dcfd0c
Angela Babayan <angela.babayan.00@gmail.com>
2020-12-18 20:44:07 +0400

    adding Config class

--- core/init.php
+++ core/init.php
@@ -17,9 +17,9 @@ $GLOBALS['config'] = array(
   )
 );
 
-spl_autoload_register(function($class)){
-  require_once 'classes/' . $class . '.php'
-};
+spl_autoload_register(function($class){
+  require_once 'classes/' . $class . '.php';
+});
 
 require_once 'functions/sanitize.php';
 
