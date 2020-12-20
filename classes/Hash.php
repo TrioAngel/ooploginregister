7101a71ad506e575c5945750a9b86534e7982e39
Angela Babayan <angela.babayan.00@gmail.com>
2020-12-20 22:56:49 +0400

    registering users

--- classes/Hash.php
+++ classes/Hash.php
@@ -2,5 +2,17 @@
 
 
 class Hash {
+  public static function make($string, $salt = ''){
+    return hash('sha256', $string . $salt);
+  }
+
+  public static function salt($length){
+    return random_bytes($length);
+  }
+
+  public static function uniqe(){
+    return self::make(uniqid());
+  }
+
 
 }
